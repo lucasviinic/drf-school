ef79cb0f1f0f3a63684282c2402a85fe59a50fed
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-10-16 18:37:27 -0300

    Testando GET e POST

--- services/escola/test/tests_cursos.py
+++ services/escola/test/tests_cursos.py
@@ -1,6 +1,8 @@
+from django.http import response
 from rest_framework.test import APITestCase
 from escola.models import Curso
 from django.urls import reverse
+from rest_framework import status
 
 
 class CursosTestCase(APITestCase):
@@ -13,5 +15,41 @@ class CursosTestCase(APITestCase):
             codigo_curso='CT2', descricao='Curso teste 2', nivel='A'
         )
 
-    def test_falhador(self):
-        self.fail('O teste falhou de proposito, não se preocupe :)')
+    # def test_falhador(self):
+    #     self.fail('O teste falhou de proposito, não se preocupe :)')
+
+    def test_requisicao_get_para_listar_cursos(self):
+        '''Teste para verificar a requisição GET para listar os cursos'''
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_post_para_criar_curso(self):
+        '''Teste para verificar a requisição POST para criar um curso'''
+        data = {
+            'codigo_curso': 'CTT3',
+            'descricao': 'Curso teste 3',
+            'nivel': 'A'
+        }
+        response = self.client.post(self.list_url, data=data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
